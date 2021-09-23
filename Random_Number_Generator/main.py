d78fc5ed4289b6b82abac978265766ba4b981231
Jose Chavez <jac745@txstate.edu>
2021-09-22 20:51:45 -0500

    Update main.py

--- Random_Number_Generator/main.py
+++ Random_Number_Generator/main.py
@@ -2,10 +2,11 @@
 import random
 
 # function that prints an list of 5 random numbers from 1 to 70
-def random_numbers():
-    n = random.sample(range(1, 71), 5) #random.sample selects unique values (nonrepetitive)
+def random_numbers(num):
+    n = random.sample(range(1, num), 5) #random.sample selects unique values (nonrepetitive)
     print(n)
 
 # main
 if __name__ == '__main__':
-    random_numbers()
+    number = int(input("How many numbers are in your range?")) # Ask user for the range of numbers
+    random_numbers(number)
