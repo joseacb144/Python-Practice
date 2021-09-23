1caa127c8e4037daf7543b7c78fe4aea293939c6
Jose Chavez <jac745@txstate.edu>
2021-09-22 21:11:15 -0500

    Update main.py

--- Random_Number_Generator/main.py
+++ Random_Number_Generator/main.py
@@ -2,16 +2,33 @@
 import random
 
 # function that prints an list of 5 random numbers from 1 to 70
-def random_numbers(num, a_num=1, v_num=1):
-    for i in range(a_num):
+def random_numbers(num=1, l_num=1, v_num=1):
+    for i in range(l_num):
         n = random.sample(range(1, num+1), v_num) #random.sample selects unique values (nonrepetitive)
         print(n)
 
-# main
+def megaball(mega_num=1, l_num=1):
+    for i in range(l_num):
+        m = random.sample(range(1, mega_num+1), 1)
+        print(m)
+
+# --------- MAIN ---------
 if __name__ == '__main__':
     number = int(input("How many numbers are in your range?\n")) # Ask user for the range of numbers
-    array_num = int(input("How many lists of numbers do you need?\n"))
-    value_num = int(input("How many numbers do you need in each list?\n"))
+    list_num = int(input("How many lists of numbers do you need?\n")) # Ask user for the amount of list
+    value_num = int(input("How many numbers do you need in each list?\n")) # Ask user how many numbers in each list
 
-    # Run function
-    random_numbers(number, array_num, value_num)
+    mega_bool = input("Do you need megaball numbers? (YES/NO)\n").upper()
+    if mega_bool == 'YES':
+        mega_range = int(input("How many numbers are in your megaball range?\n"))
+
+        print("These are your lottery numbers: ")
+        random_numbers(number, list_num, value_num)
+        print("These are your megaball numbers: ")
+        megaball(mega_range, list_num)
+        print("Good Luck!")
+    else:
+        # Run function
+        print("These are your lottery numbers: ")
+        random_numbers(number, list_num, value_num)
+        print("Good Luck!")
