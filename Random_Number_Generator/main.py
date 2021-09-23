9ef18740987ae45554af20a7bd3ff0aa5af8b34b
Jose Chavez <jac745@txstate.edu>
2021-09-22 20:44:38 -0500

    Update main.py

--- Random_Number_Generator/main.py
+++ Random_Number_Generator/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# random is needed to use random.sample()
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# function that prints an list of 5 random numbers from 1 to 70
+def random_numbers():
+    n = random.sample(range(1, 71), 5) #random.sample selects unique values (nonrepetitive)
+    print(n)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# main
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    random_numbers()
