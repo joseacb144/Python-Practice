250a9ce20e26ece2b88fb749bb2ef7fdf894ca4e
Jose Chavez <jac745@txstate.edu>
2021-10-12 10:03:58 -0500

    Update main.py

--- Math_Game/main.py
+++ Math_Game/main.py
@@ -1,6 +1,5 @@
 import random
 import os
-
 random.seed()
 
 
@@ -19,32 +18,15 @@ def symbol_pro():
 def _symbol():
     return random.choice(["", "-"])
 
-
-def beginner_game():
+def game(difficulty):
     question_count = 1
     score = 0
+    math_question = ''
     while question_count <= 10:
-        math_question = f'{number()} {symbol()} {number()}'
-        math_answer = eval(math_question)
-        print(f'Question {question_count}: {math_question}')
-
-        player_answer = int(input("What is the answer? "))
-        if player_answer == math_answer:
-            print(f"Correct! The answer is: {math_answer} \n")
-            score += 1
-            question_count += 1
+        if difficulty == 1:
+            math_question = f'{number()} {symbol()} {number()}'
         else:
-            print(f"That is incorrect. The answer is: {math_answer}.\n")
-            question_count += 1
-
-    print(f'Your total score is: {score}.')
-
-
-def pro_game():
-    question_count = 1
-    score = 0
-    while question_count <= 10:
-        math_question = f'{_symbol()} {number()} {symbol_pro()} {number()} {symbol_pro()} {number()}'
+            math_question = f'{_symbol()} {number()} {symbol_pro()} {number()} {symbol_pro()} {number()}'
         math_answer = eval(math_question)
         print(f'Question {question_count}: {math_question}')
 
@@ -65,9 +47,11 @@ if __name__ == '__main__':
     print('Hello, you get 10 questions to answer!\n')
     player_decision = int(input("Select 1 for beginner or select 2 for pro.\n"))
 
-    if player_decision == 1:
-        beginner_game()
-    elif player_decision == 2:
-        pro_game()
+    # if player_decision == 1:
+    #     beginner_game()
+    # elif player_decision == 2:
+    #     pro_game()
+
+    game(player_decision)
 
     os.system('pause')
