83f2a9fc937c462a95c9493f89f817ccfe9bbbe0
Jose Chavez <jac745@txstate.edu>
2021-10-12 13:54:07 -0500

    Update main.py

--- Math_Game/main.py
+++ Math_Game/main.py
@@ -4,7 +4,6 @@ import os
 
 random.seed()
 
-
 def number():
     return random.randint(0, 10)
 
@@ -24,40 +23,62 @@ def _symbol():
 def game(difficulty):
     question_count = 1
     score = 0
-    game_time = 10 # Used for PRO. PRO gives players 10 seconds to answer
+    game_time = 10  # Used for PRO. PRO gives players 10 seconds to answer
 
     while question_count <= 10:
-        if difficulty == 1: # BEGINNER
+        if difficulty == 1:  # BEGINNER
             math_question = f'{number()} {symbol()} {number()}'
-        else: #PRO
+        elif difficulty == 2:  # PRO
             math_question = f'{_symbol()} {number()} {symbol_pro()} {number()} {symbol_pro()} {number()}'
-            initial_time = time() # Used to grab the initial time after the question is given in PRO.
+            initial_time = time()  # Used to grab the initial time after the question is given in PRO.
         math_answer = eval(math_question)
         print(f'Question {question_count}: {math_question}')
 
-        player_answer = int(input("What is the answer? "))
-        time_taken = (time() - initial_time) # Used to identify how many seconds the player used to answer in PRO.
-        intime = time_taken < game_time # Checks to see if the player answered in time in PRO.
-        if intime:
+        if difficulty == 1:
+            player_answer = int(input("What is the answer? "))
             if player_answer == math_answer:
                 print(f"Correct! The answer is: {math_answer} \n")
                 score += 1
                 question_count += 1
-                print(game_time-time_taken) # 10 seconds - the amount of seconds taken to answer
             else:
                 print(f"That is incorrect. The answer is: {math_answer}.\n")
                 question_count += 1
-                print(game_time - time_taken)
-        else:
-            print("Out of time!")
-            break
+
+        elif difficulty == 2:
+            player_answer = int(input("What is the answer? "))
+            time_taken = (time() - initial_time)  # Used to identify how many seconds the player used to answer in PRO.
+            intime = time_taken < game_time  # Checks to see if the player answered in time in PRO.
+            if intime:
+                if player_answer == math_answer:
+                    print(f"Correct! The answer is: {math_answer} \n")
+                    score += 1
+                    question_count += 1
+                    print(game_time - time_taken)  # 10 seconds - the amount of seconds taken to answer (to see how much time was left)
+                else:
+                    print(f"That is incorrect. The answer is: {math_answer}.\n")
+                    question_count += 1
+                    print(game_time - time_taken)
+            else:
+                print("Out of time!")
+                break
 
     print(f'Your total score is: {score}.')
 
 
 if __name__ == '__main__':
-    print('Hello, you get 10 questions to answer!\n')
-    player_decision = int(input("Select 1 for beginner or select 2 for pro.\n"))
+
+    print('-'*59)
+    print('| Hello and welcome to the Basic Math Game by Jose Chavez |')
+    print('-'*59)
+    print('* INFO: \n- BEGINNER is basic addition and subtraction. '
+          '\n- You are given 1 point for each correct answer for a total of 10 questions.'
+          
+          '\n\n- PRO has addition, subtraction, and multiplication.'
+          '\n- You have 10 seconds to answer each question in PRO.'
+          '\n- Your score in PRO depends on how many seconds remain for each question.'
+          '\n- If you run out of time in PRO, the game ends.\n')
+
+    player_decision = int(input("Select 1 for BEGINNER or select 2 for PRO.\n"))
 
     game(player_decision)
 
